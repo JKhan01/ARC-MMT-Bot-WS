380dd0ef45b32518166fd6c94c2cd3c279f62471
shritej112 <patilshritej112@gmail.com>
2021-03-25 01:43:45 +0530

    Initial intents added.

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -7,11 +7,12 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, EventType
+from rasa_sdk.executor import CollectingDispatcher
+
 #
 # class ActionHelloWorld(Action):
 #
@@ -25,3 +26,35 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+class ValidateRestaurantForm(Action):
+    def name(self) -> Text:
+        return "details_form"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        required_slots = ["source", "destination"]
+
+        for slot_name in required_slots:
+            if tracker.slots.get(slot_name) is None:
+                # The slot is not filled yet. Request the user to fill this slot next.
+                return [SlotSet("requested_slot", slot_name)]
+
+        # All slots are filled.
+        return [SlotSet("requested_slot", None)]
+
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_details_thanks",
+                                 Source=tracker.get_slot("source"),
+                                 Destination=tracker.get_slot("destination"))
